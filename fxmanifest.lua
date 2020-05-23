8cf87e428c0d3142edc20557bd0080120f33d9a2
HiHowdy <56754672+HiHowdy@users.noreply.github.com>
2020-05-23 23:53:06 +0100

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,6 @@ games { 'rdr2', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resource *will* become incompatible once RedM ships.'
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
     'server.lua',
 }
 
