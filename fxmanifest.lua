0b236f1617f67a1ed3bf46921e185e25464045fa
HiHowdy <56754672+HiHowdy@users.noreply.github.com>
2020-06-18 13:59:55 +0100

    Updates
    Can now steal cars from locals and get keys but holding them up.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,13 @@ games { 'rdr2', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resource *will* become incompatible once RedM ships.'
 
 server_scripts {
-    'server.lua',
+    'config.lua',
+    'server/main.lua',
 }
 
 client_scripts {
-    'client.lua',
+    'config.lua',
+    'client/main.lua',
 }
 
 export "givePlayerKeys"
